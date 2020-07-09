a59c9e682002c09cc2c32deef7c45935877d2d80
Michael Condori Mavila <majayesh2017@gmail.com>
2020-07-09 14:59:08 -0500

    Método para determinar aprobación listo

---
+++ aprobado.py
@@ -0,0 +1,11 @@
+#Autor: Michael Condori Mavila
+#Ejercicio para ver si un estudiante está aprobado o no
+
+def determinaraprobado(promedio):
+    
+    if (promedio>=10.5):
+        resultado="El alumno está aprobado."
+    else:
+        resultado="El alumno está desaprobado."
+        
+    return resultado
